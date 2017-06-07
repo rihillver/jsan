38bc866646ce5cf0216246771466b01d79707e72
rihillver <rihillver@163.com>
2017-06-07 10:40:07 +0800

    修改Render注释

--- src/main/java/com/jsan/mvc/annotation/Render.java
+++ src/main/java/com/jsan/mvc/annotation/Render.java
@@ -19,22 +19,23 @@ import com.jsan.mvc.resolve.Resolver;
 public @interface Render {
 
 	/**
-	 * 解析器名，解析器的全限定名（可以通过静态方法的方式简化，例如：FORWARD、REDIRECT、HTML、TEXT、JSON、JSONP、STREAM、BYTE、JPG、PNG、GIF
-	 * 等）。
+	 * 应用解析器，使用解析器的注册名，如果解析器的实现类没有定义注册名则使用其全限定类名。
+	 * <p>
+	 * 可以通过静态常量匹配的方式简化，例如：FORWARD、REDIRECT、HTML、TEXT、JSON、JSONP、STREAM、BYTE、JPG、PNG、GIF等。
 	 * 
 	 * @return
 	 */
 	String value() default "";
 
 	/**
-	 * 视图。
+	 * 视图 url。
 	 * 
 	 * @return
 	 */
 	String url() default "";
 
 	/**
-	 * 解析器。
+	 * 指定解析器，自定义解析器，优先级高于 value()。
 	 * 
 	 * @return
 	 */
