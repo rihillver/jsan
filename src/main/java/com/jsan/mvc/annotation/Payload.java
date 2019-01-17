ef255b2e6bd7f538b6cb57f3532de49ed518c372
rihillver <rihillver@163.com>
2019-01-17 10:46:31 +0800

    添加Rayload注解功能

--- src/main/java/com/jsan/mvc/annotation/Payload.java
+++ src/main/java/com/jsan/mvc/annotation/Payload.java
@@ -19,7 +19,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface FormConvert {
+public @interface Payload {
 	
 	/**
 	 * 声明是否使用动态代理的方式，仅对于 Bean 的转换类型才有效，对于 Map 的转换类型无效。
@@ -29,26 +29,10 @@ public @interface FormConvert {
 	boolean value() default false;
 	
 	/**
-	 * 待实现。。。。
-	 * 是否处理客户端直接以json对象进行深度序列化后的形式提交请求参数的情况进行有效的特殊转换。
-	 * <p>
-	 * 注：这里指的不是json字符串，而是原生json对象，例如客户端通过jQuery以ajax的方式直接以json对象作为data提交时，其默认将对json对象进行深度序列化并转换成比较特殊的方式表示键值对，如items[]=[1,2,3]、obj[a]=xxx等，因为jQuery需要调用jQuery.param序列化参数，jQuery.param(obj, traditional)默认情况下traditional为false，即jquery会深度序列化参数对象，以适应如PHP和Ruby on Rails框架，但servelt无法处理，当然了，客户端可以通过设置traditional为true阻止深度序列化。
-	 * 
-	 * @return
-	 */
-	boolean deep() default false;
-	
-	/**
 	 * 指定表单字段的前缀。
 	 * 
 	 * @return
 	 */
-	String prefix() default "";
+	String encoding() default "";
 
-	/**
-	 * 指定参与表单转换的参数。
-	 * 
-	 * @return
-	 */
-	String[] params() default {};
 }
